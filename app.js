b73f330fc110032d8412f7c712010d18b16154df
Michał <michal.bielawski@hotmail.com>
2023-08-24 14:55:55 +0200

    added post method

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const express = require("express")
 const gunzData = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/gunz-data.json`)
 )
+
+
 const app = express();
+///// MIDDLEWARE --> gets body parameteres
+app.use(express.json())
 
 
 const port = 3000;
@@ -21,6 +25,19 @@ app.get("/api/v1/guns", (req, res)=>{
             guns: gunzData
         }
     })
+})
+
+app.post("/api/v1/guns", (req, res)=>{
+    const newId = gunzData[gunzData.length - 1].id + 1;
+    const newGun = {id: newId, ...req.body}
+    gunzData.push(newGun)
+    fs.writeFile(`${__dirname}/dev-data/data/gunz-data.json`, JSON.stringify(gunzData), err=>{
+        res.status(201).json({
+            status: "Success",
+            gun: newGun
+        })
+    })
+    
 })
 
 
