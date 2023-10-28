413dd76ea244e68fb6431ff3d23b89ab26ca3f52
Michał <michal.bielawski@hotmail.com>
2023-10-28 17:41:22 +0200

    refactoring cart page

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -2,12 +2,27 @@ import React from 'react'
 import Navbar from './Navbar'
 
 const Cart = () => {
+
+  /// TO REFACTOR
+  const [cartItems, setCartItems] = React.useState([])
+    
+    Object.keys(localStorage).map(k => {
+      setCartItems(prevState=>[
+        ...prevState,
+        JSON.parse(localStorage.getItem(k))
+      ])
+    }
+    )
+
+    console.log("I FIRE ONCE")
+    console.log(cartItems)
+  
   return (
     <div>
         <Navbar/>
         <div className="cart--body">
             <div className="cart--items">
-
+              <button onClick={console.log(cartItems)}>HEHEHEH</button>
             </div>
             <div className="cart--summary">
 
