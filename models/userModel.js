19c07f9e7aa8786dc58e3e4354c5c7637a860740
Michał <michal.bielawski@hotmail.com>
2024-02-12 14:02:07 +0100

    added password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
+const bcrypt = require("bcryptjs")
 
 
 
@@ -35,6 +36,15 @@ const userSchema = new mongoose.Schema({
 
 })
 
+userSchema.pre("save", async function(next){
+    // Only run this function if password was modified
+    if(!this.isModified("password")) return next();
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    // Delete password confirm field
+    this.passwordConfirm = undefined;
+    next();
+})
 
 const User = mongoose.model("User", userSchema)
 
