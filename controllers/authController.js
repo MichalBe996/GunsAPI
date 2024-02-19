a40a83ccf52c88a13adbab9a980b9845cc3a371c
Michał <michal.bielawski@hotmail.com>
2024-02-19 15:22:12 +0100

    added json web token for signing up/logging in users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,24 @@
 const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
 
 
 
 
 exports.signup = async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+
     try {
         res.status(201).json({
             status: "Success",
+            token,
             data: {
                 user: newUser
             }
