b0a5585bb6051d5409dba0ca523aed03227fb5a8
Michał <michal.bielawski@hotmail.com>
2024-05-14 16:18:02 +0200

    added getting all users without password/token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const Gun = require("../models/gunModel")
+const User = require("../models/userModel")
 const APIFeatures = require("../utils/apiFeatures")
 
 
@@ -6,11 +6,25 @@ const APIFeatures = require("../utils/apiFeatures")
 
 
 
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: "Error",
-        message: "This route is not yet implemented"
-    })
+exports.getAllUsers = async (req, res) => {
+    
+    try{
+        const allUsers = await User.find()
+        res.status(200).json({
+            status: "Success",
+            results: allUsers.length,
+            data: {
+              allUsers
+            }
+          })
+    }
+    
+     catch (err){
+      res.status(404).json({
+        status: "Fail",
+        message: err
+      })
+    }
 }
 exports.getUser = (req, res) => {
     res.status(500).json({
