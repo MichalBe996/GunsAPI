e88f5c9e4021c240e69f056c2f1d512d0bf73df1
Michał <michal.bielawski@hotmail.com>
2024-06-17 14:15:00 +0200

    added verification of the token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
+const { promisify } = require("util")
 const User = require("../models/userModel")
 const jwt = require("jsonwebtoken")
 const AppError = require("../utils/appError")
+
 
 
 const signToken = id => {
@@ -78,11 +80,14 @@ exports.protect = async (req, res, next) => {
         token = req.headers.authorization.split(" ")[1];
         
     }
-    console.log(token)
+    
     if(!token){
         return next(new AppError("You are not logged in! Please login to gain access", 401))
     }
-    // 2) Validating the token 
+    // 2) Verificating the token (using built-in node promisify function)
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+    console.log(decoded)
+    
 
 
     // 3) Check if user still exists 
