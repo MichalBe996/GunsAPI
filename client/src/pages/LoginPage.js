df48f681d1c4b7a604e07ed8be423b2b1ead8242
Michał <michal.bielawski@hotmail.com>
2024-07-25 16:07:45 +0200

    implementing login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import Navbar from './Navbar';
+import axios from 'axios';
 
 const LoginPage = () => {
+    const [loginData, setLoginData] = React.useState({})
+    const [message, setMessage] = React.useState('');
+    const handleChange = (e) => {
+        const target = e.target;
+        const name = target.name;
+        const value = target.value;
+        setLoginData({
+            ...loginData,
+            [name]: value
+        })
+        
+    }
+    const handleSubmit = async (e)=> {
+        try {
+            const response = await axios.post("https://localhost:5000/api/v1/users/login", {loginData});
+            setMessage(response.data.message)
+            console.log(response)
+        } catch (error) {
+            console.error('Error logging in:', error);
+            setMessage('An error occurred');
+        }
+        
+    }
+
+    
   return (
     <div>
         <Navbar/>
         <div className="login-form-header">
             <h2>Enter your e-mail and password below to login to your account:</h2>
             <div className='form-div'>
-                <form className='form-login'>
+                <form className='form-login' onSubmit={handleSubmit}>
                     <table>
                         <tr>
                         <td className="login-label">
@@ -16,10 +42,10 @@ const LoginPage = () => {
                             </td>
                             <td className='login-textbox'>
                                 <input 
-                                onChange={null}
+                                onChange={handleChange}
                                 name="email"
                                 id="email"
-                                type="text"/>
+                                type="email"/>
                             
                             </td>
                             
@@ -30,7 +56,7 @@ const LoginPage = () => {
                             </td>
                             <td className='login-textbox'>
                                 <input 
-                                onChange={null}
+                                onChange={handleChange}
                                 name="password"
                                 id="password"
                                 type="password"/>
@@ -38,6 +64,9 @@ const LoginPage = () => {
                             </td>
                             
                         </tr>
+                        <button type='submit' className="login--button">
+                            Login
+                        </button>
                     </table>
 
                 </form>
