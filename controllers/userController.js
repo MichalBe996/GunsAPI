a44bfd9286ac7e92aca0613b910a24e808333b01
Michał <michal.bielawski@hotmail.com>
2024-11-20 15:02:32 +0100

    updated user controller for handling user's cart array

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,11 +38,26 @@ exports.createUser = (req, res) => {
         message: "This route is not yet implemented"
     })
 }
-exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: "Error",
-        message: "This route is not yet implemented"
-    })
+exports.updateUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+          //// below parameter returns new document after the update is applied
+          new: true,
+          // runValidators checks e.g. whether parameters are of desired type
+          runValidators: true
+        })
+        res.status(200).json({
+          status: "Success", 
+          data: {
+            user: user
+          }
+        })
+      } catch (err) {
+        res.status(404).json({
+          status: "Fail",
+          message: err
+        })
+      }
 }
 exports.deleteUser = (req, res) => {
     res.status(500).json({
