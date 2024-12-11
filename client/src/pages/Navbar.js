301f72e39fbcbc39e4a059560614339cfcc8ed2c
Michał <michal.bielawski@hotmail.com>
2024-12-11 15:02:06 +0100

    implemented getting the cookies through client side

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -21,6 +21,12 @@ const Navbar = () => {
   const aboutRedirect = () => {
     navigate("/about")
   }
+  const getJWTCookie = () => {
+    console.log(Cookies.get("jwt"))
+  }
+  
+  
+  
   return (
     <nav className="nav-navbar">
 
@@ -29,8 +35,10 @@ const Navbar = () => {
         <div className="header--header" onClick={homeRedirect}>
             <h1 className="header">GunsApp</h1>
             <img className="target--logo" src={target}></img>
+            
         </div>
         <div className="buttons--navbar">
+            <button onClick={getJWTCookie}>Show cookies</button>
             <button onClick={homeRedirect}>Home</button>
             <button onClick={cartRedirect}>Cart</button>
             <button onClick={registerRedirect}>Register</button>
--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const signToken = id => {
 const createAndSendToken = (user, statusCodeSuccess, statusCodeError, res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: false
     }
     try {
         const token = signToken(user._id)
